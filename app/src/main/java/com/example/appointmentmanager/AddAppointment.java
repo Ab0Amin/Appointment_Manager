320aae992f17573a1fe4e93a816a704794c62f28
ahmed amin <ahmed.amin.abd.elwadod@gmail.com>
2021-02-27 05:42:04 +0200

    close screen rotaion

--- app/src/main/java/com/example/appointmentmanager/AddAppointment.java
+++ app/src/main/java/com/example/appointmentmanager/AddAppointment.java
@@ -9,31 +9,43 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import java.time.LocalDate;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
 
+import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.format.Time;
+import android.os.SystemClock;
 import android.view.KeyEvent;
+import android.view.PointerIcon;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.example.appointmentmanager.decorator.MyReceiver;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import com.google.android.material.timepicker.TimeFormat;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
 
 import java.io.FileDescriptor;
@@ -46,12 +58,19 @@ import java.text.SimpleDateFormat;
 
 import android.text.format.DateFormat;
 
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalTime;
+import org.threeten.bp.format.DateTimeFormatter;
+
 public class AddAppointment extends AppCompatActivity {
 
     //refrences from view classes
-    EditText title, note, color;
-    TextView time, selectedDates;
-    CalendarView calenderForDates;
+    EditText title, note;
+    TextView time;
+    MaterialCalendarView calenderForDates;
+    Spinner colors;
+    ImageView imageForColor;
+
 
     //refrences from classes
     Appointment newAppointment;
@@ -61,15 +80,19 @@ public class AddAppointment extends AppCompatActivity {
     DateFormat dateFormat;
     AlertDialog.Builder dlgAlert;
     Thread TimePickerThead;
+    DateTimeFormatter formatter;
+    LocalDate OpenDate;
+
 
     //variables
     int year;
     int month;
     int day;
-    String dateString;
-
+    String dateString, selectedColor, recievedDate;
+    Date selectedTime;
     //datastructure
     List<String> AllSelecteDates;
+    private LocalTime date_forDatabase;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -77,19 +100,55 @@ public class AddAppointment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appointment);
 
+
+
+        recievedDate=getIntent().getStringExtra("date");
+
         //find in activity
         title = findViewById(R.id.tx_title);
         note = findViewById(R.id.tx_notes);
-        color = findViewById(R.id.tx_color);
         time = findViewById(R.id.TX_dateFromButton);
-        selectedDates = findViewById(R.id.tx_time);
         calenderForDates = findViewById(R.id.Cl_calender);
+        colors = findViewById(R.id.spinner_colors);
+        imageForColor = findViewById(R.id.imageView_color);
+
+
         // take objects
         newAppointment = new Appointment();
         calendar = Calendar.getInstance();
         dateFormat = new DateFormat();
         AllSelecteDates = new ArrayList<>();
-         dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert = new AlertDialog.Builder(this);
+        colors.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    imageForColor.setColorFilter(Color.BLACK);
+                    selectedColor = "black";
+                } else if (position == 1){
+                    imageForColor.setColorFilter(Color.BLUE);
+                    selectedColor = "blue";
+                }
+                else if (position == 2){
+                    imageForColor.setColorFilter(Color.RED);
+                    selectedColor = "red";
+                }
+                else if (position == 3) {
+                    imageForColor.setColorFilter(Color.YELLOW);
+                    selectedColor = "yellow";
+                }
+                    else{
+                    imageForColor.setColorFilter(Color.WHITE);
+                    selectedColor = "white";
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //events
         year = calendar.get(Calendar.YEAR);
@@ -100,55 +159,81 @@ public class AddAppointment extends AppCompatActivity {
         fragmentManager = getSupportFragmentManager();
 
         //set callender
+        formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        OpenDate = LocalDate.now();
+        if ( getIntent().getStringExtra("date")!= null) {
+            OpenDate = LocalDate.parse(recievedDate,formatter);
+        }
         calendar.set(Calendar.YEAR, year);
         calendar.set(Calendar.MONTH, month);
         calendar.set(Calendar.DAY_OF_MONTH, day);
-        calenderForDates.setMinDate(calendar.getTimeInMillis() - 1000 * 60 * 60 * 12);
-        calenderForDates.setDate(calendar.getTimeInMillis());
-        calenderForDates.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        calenderForDates.state().edit().setMinimumDate(LocalDate.now()).commit();
+        calenderForDates.setSelectedDate(OpenDate);
+
+
+//add date which will open on
+        formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+        AllSelecteDates.add(OpenDate.toString());
+
+
+        calenderForDates.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR, year);
-                calendar.set(Calendar.MONTH, month);
-                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                dateString = dateFormat.format("yyyy-MMMM-dd", calendar.getTime()).toString();
-                selectedDates.append(dateString + "\n");
-                AllSelecteDates.add(dateString);
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+
+                dateString = date.getDate().toString();
+                if (AllSelecteDates.contains(dateString)) {
+                    AllSelecteDates.remove(AllSelecteDates.indexOf(dateString));
+
+                } else {
+                    AllSelecteDates.add(dateString);
+                }
             }
         });
+
+
+        imageForColor.setColorFilter(Color.BLUE);
+
+        //notifications
 
 
     }
 
     //methods
     public void insert(View view) {
-        if (AllSelecteDates.size()>0&&!color.getText().toString().isEmpty()&&!title.getText().toString().isEmpty()&&!time.getText().toString().isEmpty()) {
+        if (AllSelecteDates.size() > 0 && !title.getText().toString().isEmpty() && !time.getText().toString().isEmpty()) {
             for (int i = 0; i < AllSelecteDates.size(); i++) {
-                String c = title.getText().toString();
-                String c1 = time.getText().toString();
-                String c2 = color.getText().toString();
-                newAppointment.color = color.getText().toString();
+                date_forDatabase = LocalTime.of(selectedTime.getHours(), selectedTime.getMinutes());
+                 formatter = DateTimeFormatter.ISO_LOCAL_TIME;
+                newAppointment.color = selectedColor;
                 newAppointment.note = note.getText().toString();
                 newAppointment.title = title.getText().toString();
-                newAppointment.date_time = AllSelecteDates.get(i) +" "+ time.getText().toString();
+//                String time2 = time.getText().toString();
+                newAppointment.date_time = AllSelecteDates.get(i) + "T" + date_forDatabase.format(formatter);
                 AppointmentManagerDatabase.getInstance(this).appointmentDAO2().insertAppointment(newAppointment);
-                Toast.makeText(this, "Appointment Saved", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Appointment Saved", Toast.LENGTH_SHORT).show();
+
+//                Intent in = new Intent(this, MyReceiver.class);
+//            in.putExtra("id",i+10);
+//                PendingIntent pe = PendingIntent.getBroadcast(this,0,in,0);
+//                AlarmManager alarmManager=(AlarmManager) getSystemService(ALARM_SERVICE);
+//                alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, (SystemClock.elapsedRealtime()+5200),pe);
+
+
             }
+            onBackPressed();
 
-        }
-        else Toast.makeText(this, "Please enter a valid data", Toast.LENGTH_LONG).show();
-       }
+        } else Toast.makeText(this, "Please enter a valid data", Toast.LENGTH_LONG).show();
 
-    public void getDate(View view) {
-//        datePicker.show();
+
     }
+
 
     public void timrDialog(View view) {
 //        timePicker.show();
         TimePickerThead = new Thread(new Runnable() {
             @Override
             public void run() {
-                picker = new MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H).setHour(12).setMinute(0).setTitleText("Select Appointment time").setInputMode(MaterialTimePicker.INPUT_MODE_KEYBOARD).build();
+                picker = new MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H).setHour(11).setMinute(0).setTitleText("Select Appointment time").setInputMode(MaterialTimePicker.INPUT_MODE_KEYBOARD).build();
                 picker.show(fragmentManager, "tag");
                 picker.addOnPositiveButtonClickListener(new View.OnClickListener() {
                     @Override
@@ -160,7 +245,8 @@ public class AddAppointment extends AppCompatActivity {
                         calendar.set(Calendar.MINUTE, picker.getMinute());
                         calendar.set(Calendar.SECOND, 0);
                         calendar.set(Calendar.MILLISECOND, 0);
-                        Date selectedTime = calendar.getTime();
+
+                        selectedTime = calendar.getTime();
                         dateString = dateFormat.format("hh.mm aa", selectedTime).toString();
                         time.setText(dateString);
                     }
@@ -174,9 +260,7 @@ public class AddAppointment extends AppCompatActivity {
 
     }
 
-    public void clearSelectedDates(View view) {
-        selectedDates.setText(null);
-    }
+
 
     public void back(View view) {
         dlgAlert.setMessage("ARE YOU SURE TO DISCARD THIS APPOINTMENT");
@@ -198,6 +282,16 @@ public class AddAppointment extends AppCompatActivity {
         });
         dlgAlert.create().show();
 
-       //
+        //
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void test(View view) {
+       PointerIcon m= calenderForDates.getPointerIcon();
+        calenderForDates.postInvalidateOnAnimation();
+//        calenderForDates.get
+//        selectedTime
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+
     }
 }
